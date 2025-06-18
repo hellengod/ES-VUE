224b23a6cf4dc34aa287bc83cfbc5b6c1fc961ff
Hellen Bianchini <hellenbiangod@gmail.com>
2025-06-18 14:30:06 -0300

    feat: adicionar componente Tag com estilos personalizados e lógica condicional

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -23,7 +23,7 @@ export default {
             </span>
             <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
                 <li v-for="ingrediente in ingredientes" :key="ingrediente">
-                    <Tag :texto="ingrediente" />
+                    <Tag :texto="ingrediente" ativa/>
                 </li>
 
             </ul>
